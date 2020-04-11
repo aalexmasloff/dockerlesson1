c5bf2113c1c3cba90d87675c9c202228d87b75b4
Maslov Alex <alexmasloff@gmail.com>
2020-04-11 16:50:39 +0300

    Added project from scratch.

---
+++ WebApplicationInDocker/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationInDocker.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                result = "OK"
+            });
+        }
+    }
+}
